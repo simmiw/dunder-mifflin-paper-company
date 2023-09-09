9b7b4307247903a30424824d09eb24ff7521fec8
Simmi Walia <simmiw1994@gmail.com>
2023-09-09 23:33:18 +0200

    Added Dunder Mifflin Scanton branch departement

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,13 +1,16 @@
+import data from "../../data/data.json";
+
 export default function Team() {
+  const set = new Set();
+  data.employees.forEach((e) => set.add(e.department));
+  const departments = [...set];
+
   return (
-    <main>
-      <h2>Teams</h2>
-      <h2>Regional Manager</h2>
-      <h3>Sales Team</h3>
-      <h3>Account Team</h3>
-      <h3>Reception</h3>
-      <h3>Quality Assurance</h3>
-      <h3>HR</h3>
-    </main>
+    <>
+      <h2>Dunder Mifflin Scanton Team</h2>
+      {departments.map((dept) => {
+        return <h3>{dept}</h3>;
+      })}
+    </>
   );
 }
