afc5a4b614da18d00a45fe28b45633678011e85a
Simmi Walia <simmiw1994@gmail.com>
2023-12-23 14:46:03 +0100

    Added common file for endpoints

--- app/team/departments.tsx
+++ app/team/departments.tsx
@@ -2,31 +2,29 @@ import Link from "next/link";
 import { Suspense } from "react";
 import Loading from "../loading";
 import useSWR from "swr";
+import endpoints from "@/endpoints";
 
 export default async function Team() {
-  const url = "http://localhost:4000/employees";
-  const { data, error } = useSWR(url);
+  const { data, error } = useSWR(endpoints.employees());
+  if (error) return "Something went wrong";
 
   const set = new Set<string>();
   data?.forEach((e: { department: string }) => set.add(e.department));
   const departments = Array.from(set);
-
-  if (error) return "Something went wrong";
 
   return (
     <>
       <h2 className="text-center mb-7" key={"hello"}>
         Dunder Mifflin Scanton Team
       </h2>
-      {<Suspense fallback={<Loading />}></Suspense>}
       {departments
         ? departments.map((dept) => {
-            return (
-              <Link href={`/team/${dept}`}>
-                <h3 className="text-center mt-2">{dept}</h3>
-              </Link>
-            );
-          })
+          return (
+            <Link href={`/team/${dept}`}>
+              <h3 className="text-center mt-2">{dept}</h3>
+            </Link>
+          );
+        })
         : null}
     </>
   );
